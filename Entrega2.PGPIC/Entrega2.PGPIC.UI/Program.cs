0459a33af501bb4af2b89395b6f6d76f081c8a7b
Diana García <dianagarcia1128355@correo.itm.edu.co>
2024-05-01 23:44:51 -0500

    Add custom styles, logo images, fix launchsettings, add footer component

--- Entrega2.PGPIC/Entrega2.PGPIC.UI/Program.cs
+++ Entrega2.PGPIC/Entrega2.PGPIC.UI/Program.cs
@@ -11,7 +11,7 @@ namespace Entrega2.PGPIC.UI
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7000") });
 
             await builder.Build().RunAsync();
         }
